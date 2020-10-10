14f8d7a51e0c609f39db19fb361a68c0af1bf1a6
gigous <bgigous@protonmail.com>
2020-10-09 20:30:30 -0600

    Add instructions, remove commands

--- todolist.py
+++ todolist.py
@@ -4,15 +4,3 @@ from app import create_app
 
 app = create_app("development")
 
-
-@app.cli.command()
-def test():
-    pass
-
-
-@app.cli.command('fill_db')
-def fill_db():
-    """Fills database with random data."""
-
-    # Import your functions here
-    #from utils.fake_generator import
